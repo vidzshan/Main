878f76bd996f69d252ed2d7cd834b36cef912545
vidushan <prabashvidushan@gmail.com>
2024-02-21 04:06:14 +0530

    Spring boot backend crud using dependencies as JPA, spring web and spring data JPA

---
+++ demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
